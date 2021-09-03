e50e294c100d8fc4347ee327b2c512b193516889
olio <cl3avergr33n3@protonmail.com>
2021-09-03 11:16:54 +0200

    Fixed minor bug, added comments

--- options.js
+++ options.js
@@ -1,22 +1,22 @@
 function saveOptions(e) {
 	e.preventDefault();
 	browser.storage.local.set({"settings":
-	{
+		{
 		archive_password: document.querySelector("#arch_pass").value,
 		yara_rules_url: document.querySelector("#yara_url").value
-	}
+		}
 	});
 }
 
 function restoreOptions() {
 
 	function setCurrentChoice(result) {
-	document.querySelector("#arch_pass").value = result.settings.archive_password || "infected";
-	document.querySelector("#yara_url").value = result.settings.yara_rules_url;
+		document.querySelector("#arch_pass").value = result.settings.archive_password;
+		document.querySelector("#yara_url").value = result.settings.yara_rules_url;
 	}
 
 	function onError(error) {
-	console.log(`Error: ${error}`);
+		console.log(`Error: ${error}`);
 	}
 
 	let getting = browser.storage.local.get("settings");
